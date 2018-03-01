674faacedd71c4f3e8bb959f30f1f57364f58ba5
misakar <neo1218@yeah.net>
2018-03-01 19:55:06 +0800

    update

--- app.py
+++ app.py
@@ -26,29 +26,34 @@ def ccnu_loop(f):
 @app.listener('before_server_start')
 async def init_dataset(app, loop):
     # 初始化数据集
-    ## 1. 删除错误数据
     ccnu = app.config.CCNU
-    drop_indexes = ccnu[
-        #--------------------------------------------------
-        (   ccnu['orgName'].str.contains("超市"))         |
-        (   ccnu['orgName'].str.contains("保卫处"))       |
-        (   ccnu['orgName'].str.contains("后勤处"))       |
-        (   ccnu['orgName'].str.contains("信息化办公室")) |
-        (   ccnu['orgName']      ==      '华中师范大学')  |
-        (   ccnu['transMoney'] <= 0                     ) |
-        (   ccnu['transMoney'] >= 90                    )#|
-        #--------------------------------------------------
-    ].index
-    ccnu = ccnu.drop(drop_indexes)
-    ## 2. 处理食堂窗口名称
+    # drop_indexes = ccnu[
+    #     #--------------------------------------------------
+    #     (   ccnu['orgName'].str.contains("超市"))         |
+    #     (   ccnu['orgName'].str.contains("保卫处"))       |
+    #     (   ccnu['orgName'].str.contains("后勤处"))       |
+    #     (   ccnu['orgName'].str.contains("信息化办公室")) |
+    #     (   ccnu['orgName']      ==      '华中师范大学')  |
+    #     (   ccnu['transMoney'] <= 0                     ) |
+    #     (   ccnu['transMoney'] >= 90                    )#|
+    #     #--------------------------------------------------
+    # ].index
+    # ccnu = ccnu.drop(drop_indexes)
+    ## 1. 删除错误数据
     canteen = ccnu[ ccnu['orgName'].str.contains("饮食中心") ]
+    canteen = canteen.drop(canteen[
+        ( canteen['orgName'].str.contains("超市") ) |
+        ( canteen['transMoney'] <= 0 ) |
+        ( canteen['transMoney'] >= 90 )
+    ].index)
+    ## 2. 处理食堂窗口名称
     canteen['orgName'] = canteen['orgName'].apply(
         lambda x: x.split('/')[3] + '&' + x.split('/')[-1]
     )
     ## 3. 食堂窗口名称分词
     orgName = canteen.groupby('orgName').size().reset_index(name="value") # value; sort
     orgName['words_in_orgName'] = orgName['orgName'].apply(
-        lambda x: ' '.join(jieba.cut(x.split('&')[1], cut_all=True))
+        lambda x: ' '.join(jieba.cut(x.split('&')[1]))
     )
     ## 4. 计算食堂窗口名称tf-idf矩阵
     tfidf = TfidfVectorizer()
--- recommender.py
+++ recommender.py
@@ -1,3 +1,4 @@
+import aiohttp
 from app import app, ccnu_loop, executor, json, pd
 
 @app.route('/api/recommender/', methods=['GET'])
@@ -13,10 +14,66 @@ async def recommender(request, canteen, cos, loop):
     recommends = await loop.run_in_executor(
         executor, handle_recommendations, orgName, cos, orgs
     )
-    return json(recommends)
+    borgs, bindices = recommends['breakfast']
+    lorgs, lindices = recommends['lunch']
+    dorgs, dindices = recommends['dinner']
+    borgs = borgs[0].tolist()
+    lorgs = lorgs[0].tolist()
+    dorgs = dorgs[0].tolist()
+    """
+    bsrcs = []; lsrcs = []; dsrcs = []
+    for borg in borgs:
+        try:
+            bsrc = await get_src(borg)
+        except:
+            bsrc = "https://avatars0.githubusercontent.com/u/23288182?s=460&v=4"
+        bsrcs.append(bsrc)
+    for lorg in lorgs:
+        try:
+            lsrc = await get_src(lorg)
+        except:
+            lsrc = "https://avatars0.githubusercontent.com/u/23288182?s=460&v=4"
+        lsrcs.append(lsrc)
+    for dorg in dorgs:
+        try:
+            dsrc = await get_src(dorg)
+        except:
+            dsrc = "https://avatars0.githubusercontent.com/u/23288182?s=460&v=4"
+        dsrcs.append(dsrc)
+    print(len(bsrcs))
+    """
+    bsrcs = lsrcs = dsrcs = ["", "", ""]
+    json_data = {
+        'breakfast': {
+            'head': "早餐你的最爱是 " + orgs[0] + ",推荐你尝尝以下窗口",
+            'recommends': json_format(borgs, bindices, bsrcs)
+        },
+        'lunch': {
+            'head': "中餐你的最爱是 " + orgs[1] + ",推荐你尝尝以下窗口",
+            'recommends': json_format(lorgs, lindices, lsrcs)
+        },
+        'dinner': {
+            'head': "晚餐你的最爱是 " + orgs[2] + ",推荐你尝尝以下窗口",
+            'recommends': json_format(dorgs, dindices, dsrcs)
+        }
+    }
+    return json(json_data)
+
+async def get_src(org):
+    import json
+    imgapi = "https://image.baidu.com/search/acjson?tn=resultjson_com&ipn=rj&ie=utf-8&oe=utf-8&word=%s"
+    src = ""
+    async with aiohttp.ClientSession() as session:
+        async with session.get(imgapi % org.split('&')[1], timeout=3) as resp:
+            data = await resp.text()
+            json_data = json.loads(data)
+            src = json_data['data'][0]['thumbURL']
+    return src
 
 # cpu-intensive function
 def handle_recommendations(orgName, cos, orgs):
+    # 食堂窗口推荐函数
+    # 根据余弦相似度推荐相似度较高的窗口
     bindex = orgName[ (orgName['orgName'] == orgs[0]) ].index[0]  # breakfast
     lindex = orgName[ (orgName['orgName'] == orgs[1]) ].index[0]  # lunch
     dindex = orgName[ (orgName['orgName'] == orgs[2]) ].index[0]  # dinner
@@ -32,53 +89,31 @@ def handle_recommendations(orgName, cos, orgs):
     bindices = [i[0] for i in bscores]
     lindices = [i[0] for i in lscores]
     dindices = [i[0] for i in dscores]
-    srcs = [
-        'https://avatars0.githubusercontent.com/u/5249050?s=460&v=4',
-        'https://avatars0.githubusercontent.com/u/5249050?s=460&v=4',
-        'https://avatars0.githubusercontent.com/u/5249050?s=460&v=4'
-    ]
+    borgs = orgName['orgName'].iloc[bindices],
+    lorgs = orgName['orgName'].iloc[lindices],
+    dorgs = orgName['orgName'].iloc[dindices],
     return {
-        'breakfast': {
-            'like': orgs[0],
-            'recommends': json_format(
-                orgName['orgName'].iloc[bindices],
-                bindices, srcs
-            )
-        },
-        'lunch': {
-            'like': orgs[1],
-            'recommends': json_format(
-                orgName['orgName'].iloc[lindices],
-                lindices, srcs
-            )
-        },
-        'dinner': {
-            'like': orgs[2],
-            'recommends': json_format(
-                orgName['orgName'].iloc[dindices],
-                dindices, srcs
-            )
-        },
+        "breakfast": (borgs, bindices),
+        "lunch": (lorgs, lindices),
+        "dinner": (dorgs, dindices)
     }
 
 def handle_orglike(sid, canteen):
     breakfast = ['06:30:00', '10:30:00']
     lunch = ['10:40:00', '14:00:00']
     dinner = ['17:00:00', '21:30:00']
-    # print(canteen.info())
-    # canteen = canteen[ (canteen['userId'] == int(sid)) ]  # 特定学生的消费记录
-    # print(canteen.head())
-    breakfast_canteen = canteen[
-        (canteen['dealDateTime'].str.split().str[1] >= breakfast[0]) &
-        (canteen['dealDateTime'].str.split().str[1] <= breakfast[-1])
+    sid_canteen = canteen[ (canteen['userId'] == int(sid)) ]  # 特定学生的消费记录
+    breakfast_canteen = sid_canteen[
+        (sid_canteen['dealDateTime'].str.split().str[1] >= breakfast[0]) &
+        (sid_canteen['dealDateTime'].str.split().str[1] <= breakfast[-1])
     ]
-    lunch_canteen = canteen[
-        (canteen['dealDateTime'].str.split().str[1] >= lunch[0]) &
-        (canteen['dealDateTime'].str.split().str[1] <= lunch[-1])
+    lunch_canteen = sid_canteen[
+        (sid_canteen['dealDateTime'].str.split().str[1] >= lunch[0]) &
+        (sid_canteen['dealDateTime'].str.split().str[1] <= lunch[-1])
     ]
-    dinner_canteen = canteen[
-        (canteen['dealDateTime'].str.split().str[1] >= dinner[0]) &
-        (canteen['dealDateTime'].str.split().str[1] <= dinner[-1])
+    dinner_canteen = sid_canteen[
+        (sid_canteen['dealDateTime'].str.split().str[1] >= dinner[0]) &
+        (sid_canteen['dealDateTime'].str.split().str[1] <= dinner[-1])
     ]
     breakfast_orgs = breakfast_canteen.groupby('orgName').size().reset_index(name="value")
     lunch_orgs = lunch_canteen.groupby('orgName').size().reset_index(name="value")
